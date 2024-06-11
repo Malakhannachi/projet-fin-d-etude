5cbef420156de4b362b2c36309214497ea1f8537
Malak Hannachi <malakhannachi@gmail.com>
2024-06-11 15:52:14 +0200

    vérifier index

--- index.php
+++ index.php
@@ -6,12 +6,20 @@ spl_autoload_register(function ($class_name) {
 });
 
 $ctrFrm = new Controller();
+$secuCtrl = new SecuriteController();
 
 if(isset($_GET['action'])) {
     switch($_GET['action']) {
         case 
         "accueil":$ctrFrm->accueil(); 
     break;
+        case
+        "login":$secuCtrl->login();
+        break;
+        "register":$secuCtrl->register(); 
+        break;
+        "logout":$secuCtrl->logout();
+          break;
         
        
 
